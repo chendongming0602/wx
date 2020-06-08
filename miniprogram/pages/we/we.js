9bd2ba07c5ff65d87756aef224fe8488ecdc0aa7
陈栋明 <chendm0602@163.com>
2020-06-08 11:14:13 +0800

    添加图片裁剪功能

---
+++ miniprogram/pages/we/we.js
@@ -0,0 +1,139 @@
+// pages/we/we.js
+import WeCropper from '../../we-cropper/we-cropper.js'
+
+const device = wx.getSystemInfoSync() // 获取设备信息
+const width = device.windowWidth // 示例为一个与屏幕等宽的正方形裁剪框
+const height = width
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    cropperOpt: {
+      id: 'cropper', // 用于手势操作的canvas组件标识符
+      targetId: 'targetCropper', // 用于用于生成截图的canvas组件标识符
+      pixelRatio: device.pixelRatio, // 传入设备像素比
+      width,  // 画布宽度
+      height, // 画布高度
+      scale: 2.5, // 最大缩放倍数
+      zoom: 8, // 缩放系数
+      cut: {
+        x: (width - 200) / 2, // 裁剪框x轴起点
+        y: (width - 200) / 2, // 裁剪框y轴期起点
+        width: 200, // 裁剪框宽度
+        height: 200 // 裁剪框高度
+      }
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const { cropperOpt } = this.data
+
+    this.cropper = new WeCropper(cropperOpt)
+      .on('ready', (ctx) => {
+        console.log(`wecropper is ready for work!`)
+      })
+      .on('beforeImageLoad', (ctx) => {
+        wx.showToast({
+          title: '上传中',
+          icon: 'loading',
+          duration: 20000
+        })
+      })
+      .on('imageLoad', (ctx) => {
+        wx.hideToast()
+      })
+  },
+  //上传按钮
+  uploadTap() {
+    const self = this
+
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success(res) {
+        const src = res.tempFilePaths[0]
+
+        self.cropper.pushOrign(src)
+      }
+    })
+  },
+  //生成按钮
+  getCropperImage() {
+    this.wecropper.getCropperImage((tempFilePath) => {
+      // tempFilePath 为裁剪后的图片临时路径
+      console.log(tempFilePath)
+      if (tempFilePath) {
+        wx.previewImage({
+          current: '',
+          urls: [tempFilePath]
+        })
+      } else {
+        console.log('获取图片地址失败，请稍后重试')
+      }
+    })
+  },
+  touchStart(e) {
+    this.cropper.touchStart(e)
+  },
+  touchMove(e) {
+    this.cropper.touchMove(e)
+  },
+  touchEnd(e) {
+    this.cropper.touchEnd(e)
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
